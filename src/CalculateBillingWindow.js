732389baa8ed330e1fc3358b0843c9e78553b759
Francis Manansala <francis.manansala@boldcommerce.com>
2021-02-21 21:11:01 -0600

    change currentDate to billCreateDate

--- src/CalculateBillingWindow.js
+++ src/CalculateBillingWindow.js
@@ -1,6 +1,6 @@
 const BillingWindow = require('../src/BillingWindow');
 
-const calculateBillingWindow = (shop, currentDate = new Date()) => {
+const calculateBillingWindow = (shop, billCreateDate = new Date()) => {
     return new BillingWindow(new Date(), new Date());
 };
 
